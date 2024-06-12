b74abaf6d6db55cfc0b91a5c750e028f48abd287
leo <lch01234@gmail.com>
2024-06-12 21:43:45 +0900

    chore: formatting

--- language.lua
+++ language.lua
@@ -3,63 +3,63 @@ local Language = {}
 local classes = {}
 
 local findfield = function(self, k)
-	local clsname = self._clsname
-	while true do
-		local cls = classes[clsname]
-		if not cls then
-			return
-		end
+  local clsname = self._clsname
+  while true do
+    local cls = classes[clsname]
+    if not cls then
+      return
+    end
 
-		local val = rawget(cls, k)
+    local val = rawget(cls, k)
 
-		if val ~= nil then
-			rawset(self, k, val)
-			return val
-		end
+    if val ~= nil then
+      rawset(self, k, val)
+      return val
+    end
 
-		clsname = cls._base
-	end
+    clsname = cls._base
+  end
 end
 
 local Object = {
-	_clsname = "Object",
-	_base = nil,
-	__index = findfield,
+  _clsname = "Object",
+  _base = nil,
+  __index = findfield,
 }
 
 classes["Object"] = Object
 
 function Language.newclass(name, supername)
-	local class = {}
-	return Language.regclass(class, name, supername)
+  local class = {}
+  return Language.regclass(class, name, supername)
 end
 
 function Language.regclass(class, name, supername)
-	if classes[name] then
-		error("class already exists :" .. name, 2)
-		return
-	end
+  if classes[name] then
+    error("class already exists :" .. name, 2)
+    return
+  end
 
-	classes[name] = class
-	class._clsname = name
-	supername = supername or "Object"
-	class._base = supername
+  classes[name] = class
+  class._clsname = name
+  supername = supername or "Object"
+  class._base = supername
 
-	local super = classes[supername]
-	setmetatable(class, super)
-	class.__index = findfield
+  local super = classes[supername]
+  setmetatable(class, super)
+  class.__index = findfield
 
-	class.new = function(...)
-		local ins = {}
-		ins._clsname = name
-		ins.__index = findfield
-		setmetatable(ins, class)
-		if ins.ctor then
-			ins:ctor(...)
-		end
-		return ins
-	end
-	return class
+  class.new = function(...)
+    local ins = {}
+    ins._clsname = name
+    ins.__index = findfield
+    setmetatable(ins, class)
+    if ins.ctor then
+      ins:ctor(...)
+    end
+    return ins
+  end
+  return class
 end
 
 return Language
